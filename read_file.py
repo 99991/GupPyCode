4d0a052209e18ccb8c770595511cdcb40087fbc2
99991 <99991@users.noreply.github.com>
2026-09-08 20:33:21 +0200

    handle file read error

--- read_file.py
+++ read_file.py
@@ -4,6 +4,8 @@ def read_file(path: str) -> dict[str, str] | str:
     ext = path.rsplit(".")[-1].lower().replace("jpg", "jpeg")
     if ext in ["png", "jpeg"]:
         data = run_bash(f"cat '{path}' | base64 -w 0", limit=False)
+        if data.startswith("[ERROR:"):
+            return data
         return {
             "type": "image",
             "data": f"data:image/{ext};base64," + data,
